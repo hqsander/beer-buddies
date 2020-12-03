59a96cadf88d3fb9a32bee40ce0ce20a1223fda3
Henrique Sander Fernandes <hqsander@gmail.com>
2020-12-03 11:23:47 -0300

    Implementa esboço da tela que lista cervejas

--- lib/telas/tela_lista_cervejas.dart
+++ lib/telas/tela_lista_cervejas.dart
@@ -1,10 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TelaListaCervejas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.grey.shade50,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.amber,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Colors.white,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.list),
+              color: Colors.amber,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.bar_chart),
+              color: Colors.grey.shade600,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text("Antuérpia"),
+            subtitle: Text("Nikita Cherry Hickey"),
+          ),
+        ],
+      ),
     );
   }
 }
