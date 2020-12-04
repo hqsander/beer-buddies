267d0bcb62e5796104c24e14b0b1493135d0e817
Henrique Sander Fernandes <hqsander@gmail.com>
2020-12-04 09:58:11 -0300

    Implementa modelo para cerveja

---
+++ lib/modelo/cerveja.dart
@@ -0,0 +1,22 @@
+class Cerveja {
+  String nome;
+  String cervejaria;
+  String estilo;
+  bool possuiIbuInformado;
+  double ibu;
+  double teor;
+  int odor;
+  int sabor;
+  int retrogosto;
+
+  Cerveja(
+      {this.nome,
+      this.cervejaria,
+      this.estilo,
+      this.possuiIbuInformado,
+      this.ibu,
+      this.teor,
+      this.odor,
+      this.sabor,
+      this.retrogosto});
+}
--- lib/telas/tela_lista_cervejas.dart
+++ lib/telas/tela_lista_cervejas.dart
@@ -1,7 +1,56 @@
+import 'package:beer_buddies/modelo/cerveja.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TelaListaCervejas extends StatelessWidget {
+  List<Cerveja> cervejas = [
+    Cerveja(
+        nome: "nome1",
+        cervejaria: "cervejaria1",
+        estilo: "estilo1",
+        possuiIbuInformado: true,
+        ibu: 40,
+        teor: 5,
+        odor: 3,
+        sabor: 3,
+        retrogosto: 3),
+    Cerveja(
+        nome: "nome2",
+        cervejaria: "cervejaria2",
+        estilo: "estilo2",
+        possuiIbuInformado: true,
+        ibu: 40,
+        teor: 5,
+        odor: 3,
+        sabor: 3,
+        retrogosto: 3),
+    Cerveja(
+        nome: "nome3",
+        cervejaria: "cervejaria3",
+        estilo: "estilo3",
+        possuiIbuInformado: true,
+        ibu: 40,
+        teor: 5,
+        odor: 3,
+        sabor: 3,
+        retrogosto: 3),
+  ];
+
+  List<Widget> montarListaCervejas(List<Cerveja> cervejas) {
+    List<Widget> tiles = [];
+
+    cervejas.forEach((cerveja) {
+      tiles.add(
+        ListTile(
+          title: Text(cerveja.cervejaria),
+          subtitle: Text(cerveja.nome),
+        ),
+      );
+    });
+
+    return tiles;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,12 +87,7 @@ class TelaListaCervejas extends StatelessWidget {
         ),
       ),
       body: ListView(
-        children: [
-          ListTile(
-            title: Text("Antuérpia"),
-            subtitle: Text("Nikita Cherry Hickey"),
-          ),
-        ],
+        children: montarListaCervejas(cervejas),
       ),
     );
   }
